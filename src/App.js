42a5054c6ae7e9904e9bd2989912a7b5c81603b4
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-08-26 16:27:52 +0600

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import MainComponents from "./components/MainComponents";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/stylesheet/styles.css";
 import "./assets/stylesheet/custom.css";
-import Demo from "./components/pages/demo/Demo";
 import { Fragment } from "react";
 
 function App() {
