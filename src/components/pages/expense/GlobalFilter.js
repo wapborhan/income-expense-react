c6170a1a656ce3fb99c08f822594917f5331eed6
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-09-07 14:52:12 +0600

    dasd

--- src/components/pages/expense/GlobalFilter.js
+++ src/components/pages/expense/GlobalFilter.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
 
-export const GlobalFilter = ( {filter, setFilter} ) =>{
-	return(
-		<div>
-			Search : {' '}
-			<input className="ml-2 input-search form-control"
-				value={filter || ''}  onChange={e => setFilter(e.target.value)} style={{width: "20%"}}
-            />
-		</div>
-	)
-} 
+export const GlobalFilter = ({ filter, setFilter }) => {
+  return (
+    <div className="d-flex justify-content-end align-items-center">
+      <span>Search : </span>
+      <br />
+      <input
+        className="ml-2 mx-2  input-search form-control"
+        value={filter || ""}
+        onChange={(e) => setFilter(e.target.value)}
+        style={{ width: "30%" }}
+      />
+    </div>
+  );
+};
